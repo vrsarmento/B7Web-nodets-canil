34ee95c76e951cf76f2ca272da0f99ad74b8b24c
Victor Rocha <vrsarmento@gmail.com>
2021-10-29 20:47:00 -0300

    Pages Search and Error 404

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express'
 
-export const search = (req: Request, res: Response) => {
+import { Pet } from '../models/pet'
+import { createMenuObject } from '../helpers/createMenuObject'
 
-  // res.render('pages/search')
+export const search = (req: Request, res: Response) => {
+  const query: string = req.query.q as string
+
+  if (!query) {
+    res.redirect('/')
+    return
+  }
+
+  const list = Pet.getFromName(query)
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query
+  })
 }
